350f55ca0c23efa5a64cb70a8c998df6c90092b4
Lucas Costa <lucasvinicius3105@gmail.com>
2023-01-11 22:31:47 -0300

    :init: iniciando o projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { CardNewComponent } from './card-new/card-new.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationBarComponent,
+    HeaderComponent,
+    FooterComponent,
+    CardNewComponent
   ],
   imports: [
     BrowserModule,
