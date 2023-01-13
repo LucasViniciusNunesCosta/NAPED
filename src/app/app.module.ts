98824860075516c6d16b269baee9cf6b9f95ee06
Lucas Costa <lucasvinicius3105@gmail.com>
2023-01-13 12:06:35 -0300

    :refactor: Estruturando os componentes e pages em pastas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { CardNewComponent } from './card-new/card-new.component';
-import { CardBigComponent } from './card-big/card-big.component';
-import { NoticiasRecentesComponent } from './noticias-recentes/noticias-recentes.component';
-import { CardLongComponent } from './card-long/card-long.component';
+import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CardNewComponent } from './components/card-new/card-new.component';
+import { CardBigComponent } from './components/card-big/card-big.component';
+import { NoticiasRecentesComponent } from './components/noticias-recentes/noticias-recentes.component';
+import { CardLongComponent } from './components/card-long/card-long.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { CardLongComponent } from './card-long/card-long.component';
     CardNewComponent,
     CardBigComponent,
     NoticiasRecentesComponent,
-    CardLongComponent
+    CardLongComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
