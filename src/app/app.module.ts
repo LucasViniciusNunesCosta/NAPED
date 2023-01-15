1a5b36e2d468204af5627f83a8d76c3e76047abc
Lucas Costa <lucasvinicius3105@gmail.com>
2023-01-15 13:06:34 -0300

    :refactor: CSS BEM e organizacao pasta cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,10 @@ import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { CardNewComponent } from './components/card-new/card-new.component';
-import { CardBigComponent } from './components/card-big/card-big.component';
+import { CardNewComponent } from './components/cards/card-new/card-new.component';
+import { CardBigComponent } from './components/cards//card-big/card-big.component';
 import { NoticiasRecentesComponent } from './components/noticias-recentes/noticias-recentes.component';
-import { CardLongComponent } from './components/card-long/card-long.component';
+import { CardLongComponent } from './components/cards//card-long/card-long.component';
 import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
