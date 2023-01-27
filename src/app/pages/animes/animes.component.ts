81c99439625f7d9f2621b79b61b4fa99ca1588d6
Lucas Costa <lucasvinicius3105@gmail.com>
2023-01-27 10:32:05 -0300

    :feat: catalogo da secao animes dinamico

--- src/app/pages/animes/animes.component.ts
+++ src/app/pages/animes/animes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import data_notices from '../../../assets/data/data_notices.json'
 
 @Component({
   selector: 'page-animes',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./animes.component.css']
 })
 export class AnimesComponent implements OnInit {
+
+  noticias_animes: any = data_notices['animes']
 
   Titulo: string = "Animes"
   Banner: string = "../../../assets/images/naruto-tobi.png"
