daef1796a577b6e0d5e709affb2761300404c427
Lucas Costa <lucasvinicius3105@gmail.com>
2023-01-27 10:33:41 -0300

    :feat: catalogo secao games dinamico

--- src/app/pages/games/games.component.ts
+++ src/app/pages/games/games.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import data_notices from '../../../assets/data/data_notices.json'
 
 @Component({
   selector: 'page-games',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./games.component.css']
 })
 export class GamesComponent implements OnInit {
+
+  noticias_games: any = data_notices['games']
 
   Titulo: string = "Games"
   Banner: string = "../../../assets/images/ffxv.webp"
